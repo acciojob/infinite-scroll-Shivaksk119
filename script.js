ba0a375dd0ade5f0529ef9377d55d1b083e79482
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-26 01:41:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
+let list = document.getElementById('infi-list');
+let listNum = 0;
+let loading = false;
+
+const addList=()=>{
+	listNum++;
+	let item = document.createElement('li');
+	item.innerText = `Item ${listNum}`;
+	list.appendChild(item);
+}
+
+for(let i=0;i<10;i++){
+	addList();
+}
+
+list.addEventListener('scroll', ()=>{
+	if(!loading && list.scrollTop + list.clientHeight >= list.scrollHeight-2){
+		addList();
+		addList();
+		setTimeout(() => loading = false, 100); 
+	}
+})
+
+
 
